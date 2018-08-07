27285f6e04e321a092211e491b4cb4941e2d9cc6
Benjamin Chauvette <bdchauvette@gmail.com>
2018-08-07 00:49:54 -0400

    refactor: DRY out ICW tests

--- test/ICW.spec.ts
+++ test/ICW.spec.ts
@@ -1,13 +1,13 @@
 import { ICW } from "../src/ICW";
 import { isEven } from "./helpers/isEven";
 import { noop } from "./helpers/noop";
-import { runOfSuite } from "./suites/runOfSuite";
 import { runFromSuite } from "./suites/runFromSuite";
+import { runOfSuite } from "./suites/runOfSuite";
 import { runConsumeSuite } from "./suites/runConsumeSuite";
 import { runFilterSuite } from "./suites/runFilterSuite";
 import { runMapSuite } from "./suites/runMapSuite";
-import { runWithIndexSuite } from "./suites/runWithIndexSuite";
 import { runRejectSuite } from "./suites/runRejectSuite";
+import { runWithIndexSuite } from "./suites/runWithIndexSuite";
 
 const bindMethod = <T>(method: keyof ICW<T>) => (
   iterable: Iterable<T> | AsyncIterable<T>,
@@ -28,100 +28,43 @@ test("is an async iterable", async () => {
   await expect(new ICW([])).toBeAsyncIterable();
 });
 
-describe("::from", () => {
+describe.each`
+  method    | args        | runSuite
+  ${"of"}   | ${[]}       | ${runOfSuite}
+  ${"from"} | ${[isEven]} | ${runFromSuite}
+`("static method $method", ({ method, args, runSuite }) => {
   test("is a function", () => {
     expect.assertions(1);
-    expect(ICW.from).toBeFunction();
+    expect((ICW as any)[method]).toBeFunction();
   });
 
   test("returns an ICW instance", () => {
     expect.assertions(1);
-    expect(ICW.from([])).toBeInstanceOf(ICW);
+    expect((ICW as any)[method](...args)).toBeInstanceOf(ICW);
   });
 
-  runFromSuite(ICW.from);
+  runSuite((ICW as any)[method]);
 });
 
-describe("::of", () => {
+describe.each`
+  method         | args        | returnInstance | runSuite
+  ${"consume"}   | ${[]}       | ${Promise}     | ${runConsumeSuite}
+  ${"filter"}    | ${[isEven]} | ${ICW}         | ${runFilterSuite}
+  ${"map"}       | ${[noop]}   | ${ICW}         | ${runMapSuite}
+  ${"reject"}    | ${[isEven]} | ${ICW}         | ${runRejectSuite}
+  ${"withIndex"} | ${[]}       | ${ICW}         | ${runWithIndexSuite}
+`("prototype method $method", ({ method, args, returnInstance, runSuite }) => {
+  // eslint-disable-next-line jest/no-identical-title
   test("is a function", () => {
     expect.assertions(1);
-    expect(ICW.of).toBeFunction();
+    expect((ICW.prototype as any)[method]).toBeFunction();
   });
 
-  test("returns an ICW instance", () => {
+  test(`returns a ${returnInstance.name}`, () => {
     expect.assertions(1);
-    expect(ICW.of()).toBeInstanceOf(ICW);
+    let icw: any = new ICW([1, 2, 3]);
+    expect(icw[method](...args)).toBeInstanceOf(returnInstance);
   });
 
-  runOfSuite(ICW.of);
-});
-
-describe("#consume", () => {
-  test("is a function", () => {
-    expect.assertions(1);
-    expect(ICW.prototype.consume).toBeFunction();
-  });
-
-  test("returns a Promise", () => {
-    expect.assertions(1);
-    expect(new ICW([]).consume()).toBeInstanceOf(Promise);
-  });
-
-  runConsumeSuite(bindMethod("consume"));
-});
-
-describe("#filter", () => {
-  test("is a function", () => {
-    expect.assertions(1);
-    expect(ICW.prototype.reject).toBeFunction();
-  });
-
-  test("returns an ICW instance", () => {
-    expect.assertions(1);
-    expect(new ICW([]).filter(isEven)).toBeInstanceOf(ICW);
-  });
-
-  runFilterSuite(bindMethod("filter"));
-});
-
-describe("#map", () => {
-  test("is a function", () => {
-    expect.assertions(1);
-    expect(ICW.prototype.map).toBeFunction();
-  });
-
-  test("returns an ICW instance", () => {
-    expect.assertions(1);
-    expect(new ICW([]).map(noop)).toBeInstanceOf(ICW);
-  });
-
-  runMapSuite(bindMethod("map"));
-});
-
-describe("#reject", () => {
-  test("is a function", () => {
-    expect.assertions(1);
-    expect(ICW.prototype.reject).toBeFunction();
-  });
-
-  test("returns an ICW instance", () => {
-    expect.assertions(1);
-    expect(new ICW([]).reject(isEven)).toBeInstanceOf(ICW);
-  });
-
-  runRejectSuite(bindMethod("reject"));
-});
-
-describe("#withIndex", () => {
-  test("is a function", () => {
-    expect.assertions(1);
-    expect(ICW.prototype.withIndex).toBeFunction();
-  });
-
-  test("returns an ICW instance", () => {
-    expect.assertions(1);
-    expect(new ICW([]).withIndex()).toBeInstanceOf(ICW);
-  });
-
-  runWithIndexSuite(bindMethod("withIndex"));
+  runSuite(bindMethod(method));
 });
