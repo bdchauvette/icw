bc4d759c28a932fbe7180ed517cc8dc6a1b2960d
Benjamin Chauvette <bdchauvette@gmail.com>
2018-08-15 00:33:28 -0400

    test: improve test suite for ICW

--- test/ICW.spec.js
+++ test/ICW.spec.js
@@ -19,17 +19,168 @@ import { runToArraySuite } from "./suites/runToArraySuite";
 import { runToPromiseSuite } from "./suites/runToPromiseSuite";
 import { runWithIndexSuite } from "./suites/runWithIndexSuite";
 
-test("returns same async iterator", () => {
-  expect.assertions(1);
-  expect(new ICW([])).toReturnSameAsyncIterator();
-});
-
 describe.each`
   method    | runSuite
   ${"of"}   | ${runOfSuite}
   ${"from"} | ${runFromSuite}
 `("static method $method", ({ method, runSuite }) => {
   runSuite(ICW[method]);
+});
+
+describe("constructor", () => {
+  test.each`
+    iterableType | iterable
+    ${"sync"}    | ${[]}
+    ${"async"}   | ${(async function*() {})()}
+  `(
+    "does not throw error when $iterableType iterable is provided",
+    ({ iterable }) => {
+      expect.assertions(1);
+      expect(() => new ICW(iterable)).not.toThrowError();
+    }
+  );
+
+  test("throws an error if no iterable is provided", () => {
+    expect.assertions(1);
+    expect(() => new ICW(null)).toThrowErrorMatchingInlineSnapshot(
+      `"First argument must implement the Iterable protocol"`
+    );
+  });
+});
+
+describe("prototype method [Symbol.asyncIterator]", () => {
+  test("returns itself", () => {
+    expect.assertions(1);
+    let icw = new ICW([]);
+    expect(icw[Symbol.asyncIterator]()).toBe(icw);
+  });
+
+  test("returns same async iterator", () => {
+    expect.assertions(1);
+    expect(new ICW([])).toReturnSameAsyncIterator();
+  });
+});
+
+describe('prototype method "next"', () => {
+  test.each`
+    iteratorType | createIterableIterator
+    ${"sync"}    | ${function*() {}}
+    ${"async"}   | ${async function*() {}}
+  `(
+    "calls method on wrapped $iteratorType iterator",
+    async ({ createIterableIterator }) => {
+      expect.assertions(1);
+
+      let iterableIterator = createIterableIterator();
+      let wrappedNext = jest.spyOn(iterableIterator, "next");
+
+      let icw = new ICW(iterableIterator);
+      await icw.next("foo");
+      expect(wrappedNext).toHaveBeenCalledWith("foo");
+    }
+  );
+});
+
+describe('prototype method "return"', () => {
+  test.each`
+    iteratorType | createIterableIterator
+    ${"sync"}    | ${function*() {}}
+    ${"async"}   | ${async function*() {}}
+  `(
+    "calls method on wrapped $iteratorType iterator, if it exists",
+    async ({ createIterableIterator }) => {
+      expect.assertions(1);
+
+      let iterableIterator = createIterableIterator();
+      let wrappedReturn = jest.spyOn(iterableIterator, "return");
+
+      let icw = new ICW(iterableIterator);
+      await icw.return("foo");
+      expect(wrappedReturn).toHaveBeenCalledWith("foo");
+    }
+  );
+
+  test.each`
+    iteratorType | iteratorSymbol
+    ${"sync"}    | ${Symbol.iterator}
+    ${"async"}   | ${Symbol.asyncIterator}
+  `(
+    "returns { done: true, value: undefined } if wrapped $iteratorType iterable does not have method",
+    async ({ iteratorSymbol }) => {
+      expect.assertions(1);
+
+      let iterable = {
+        [iteratorSymbol]: () => ({
+          next: () => ({ done: true })
+        })
+      };
+
+      let icw = new ICW(iterable);
+      let result = await icw.return("foo");
+      expect(result).toEqual({ done: true, value: undefined });
+    }
+  );
+
+  test("returns { done: true, value: undefined } if wrapped async iterable does not have method", async () => {
+    expect.assertions(1);
+
+    let iterable = {
+      [Symbol.asyncIterator]: () => ({
+        next: () => ({ done: true })
+      })
+    };
+
+    let icw = new ICW(iterable);
+    let result = await icw.return("foo");
+    expect(result).toEqual({ done: true, value: undefined });
+  });
+});
+
+describe('prototype method "throw"', () => {
+  test.each`
+    iteratorType | createIterableIterator
+    ${"sync"}    | ${function*() {}}
+    ${"async"}   | ${async function*() {}}
+  `(
+    "calls method on wrapped $iteratorType iterator, if it exists",
+    async ({ createIterableIterator }) => {
+      expect.assertions(1);
+
+      let iterableIterator = createIterableIterator();
+      let wrappedThrow = jest.spyOn(iterableIterator, "throw");
+
+      let icw = new ICW(iterableIterator);
+      let error = new Error("boom");
+
+      try {
+        await icw.throw(error);
+      } catch (_) {
+        expect(wrappedThrow).toHaveBeenCalledWith(error);
+      }
+    }
+  );
+
+  test.each`
+    iteratorType | iteratorSymbol
+    ${"sync"}    | ${Symbol.iterator}
+    ${"async"}   | ${Symbol.asyncIterator}
+  `(
+    "throws error if wrapped $iteratorType iterator does not have method",
+    async ({ iteratorSymbol }) => {
+      expect.assertions(1);
+
+      let iterable = {
+        [iteratorSymbol]: () => ({
+          next: () => ({ done: true })
+        })
+      };
+
+      let icw = new ICW(iterable);
+      let error = new Error("boom");
+
+      await expect(icw.throw(error)).rejects.toThrowError(error);
+    }
+  );
 });
 
 describe.each`
