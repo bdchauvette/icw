c3cef43e275d6b2d8a593199a4b46c9334ad97f2
Benjamin Chauvette <bdchauvette@gmail.com>
2018-09-02 22:28:44 -0400

    test: improve StrictEqual l tests for indexOf and lastIndexOf

--- test/suites/runIndexOfSuite.js
+++ test/suites/runIndexOfSuite.js
@@ -39,23 +39,12 @@ export function runIndexOfSuite(indexOf) {
 
   test.each`
     inputType          | iterableLike
-    ${"AsyncIterable"} | ${of(+0)}
-    ${"Iterable"}      | ${[+0]}
-    ${"ArrayLike"}     | ${new ArrayLike(+0)}
-    ${"Promise"}       | ${Promise.resolve(+0)}
-  `("treats -0 and +0 as equal", async ({ iterableLike }) => {
-    expect.assertions(1);
-    await expect(indexOf(iterableLike, -0)).resolves.toStrictEqual(0);
-  });
-
-  test.each`
-    inputType          | iterableLike
     ${"AsyncIterable"} | ${of()}
     ${"Iterable"}      | ${[]}
     ${"ArrayLike"}     | ${new ArrayLike()}
     ${"Promise"}       | ${Promise.resolve()}
   `(
-    "Returns -1 if $inputType input does not contain the target value",
+    "returns -1 if $inputType input does not contain the target value",
     async ({ iterableLike }) => {
       expect.assertions(1);
       await expect(
@@ -65,16 +54,39 @@ export function runIndexOfSuite(indexOf) {
   );
 
   test.each`
-    inputType          | iterableLike
-    ${"AsyncIterable"} | ${of(NaN)}
-    ${"Iterable"}      | ${[NaN]}
-    ${"ArrayLike"}     | ${new ArrayLike(NaN)}
-    ${"Promise"}       | ${Promise.resolve(NaN)}
+    inputValue           | targetValue          | expectedIndex
+    ${true}              | ${true}              | ${0}
+    ${false}             | ${false}             | ${0}
+    ${undefined}         | ${undefined}         | ${0}
+    ${null}              | ${null}              | ${0}
+    ${0}                 | ${0}                 | ${0}
+    ${0}                 | ${-0}                | ${0}
+    ${-0}                | ${-0}                | ${0}
+    ${"foo"}             | ${"foo"}             | ${0}
+    ${undefined}         | ${false}             | ${-1}
+    ${undefined}         | ${null}              | ${-1}
+    ${undefined}         | ${0}                 | ${-1}
+    ${null}              | ${false}             | ${-1}
+    ${null}              | ${0}                 | ${-1}
+    ${0}                 | ${false}             | ${-1}
+    ${""}                | ${false}             | ${-1}
+    ${""}                | ${0}                 | ${-1}
+    ${"0"}               | ${0}                 | ${-1}
+    ${"123"}             | ${123}               | ${-1}
+    ${[1, 2, 3]}         | ${"1,2,3"}           | ${-1}
+    ${[]}                | ${[]}                | ${-1}
+    ${new String("foo")} | ${new String("foo")} | ${-1}
+    ${new String("foo")} | ${"foo"}             | ${-1}
+    ${NaN}               | ${0}                 | ${-1}
+    ${NaN}               | ${"foo"}             | ${-1}
+    ${NaN}               | ${NaN}               | ${-1}
   `(
-    "Returns -1 when target value is NaN, but $iterableLike input contains NaN",
-    async ({ iterableLike }) => {
+    "uses StrictEqual comparison: ($inputValue, $targetValue) -> $expectedIndex",
+    async ({ inputValue, targetValue, expectedIndex }) => {
       expect.assertions(1);
-      await expect(indexOf(iterableLike, NaN)).resolves.toStrictEqual(-1);
+      await expect(indexOf(of(inputValue), targetValue)).resolves.toStrictEqual(
+        expectedIndex
+      );
     }
   );
 }
